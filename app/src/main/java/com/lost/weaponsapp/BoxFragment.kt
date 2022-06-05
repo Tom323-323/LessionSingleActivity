493bbb7d19bb890fa025e64fa466199a60f4fa15
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-06-05 14:15:05 +0300

    05/06 - Add bundleOf (BoxFragment.ARG_COLOR to color).

--- app/src/main/java/com/lost/weaponsapp/BoxFragment.kt
+++ app/src/main/java/com/lost/weaponsapp/BoxFragment.kt
@@ -3,4 +3,8 @@ package com.lost.weaponsapp
 import androidx.fragment.app.Fragment
 
 class BoxFragment: Fragment(R.layout.fragment_box) {
+
+    companion object{
+        const val ARG_COLOR = "color"
+    }
 }
--- app/src/main/java/com/lost/weaponsapp/RootFragment.kt
+++ app/src/main/java/com/lost/weaponsapp/RootFragment.kt
@@ -1,6 +1,33 @@
 package com.lost.weaponsapp
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.lost.weaponsapp.databinding.FragmentRootBinding
 
 class RootFragment: Fragment(R.layout.fragment_root) {
+
+    private lateinit var binding:FragmentRootBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRootBinding.bind(view)
+
+        binding.openGreen.setOnClickListener {
+            openBox(Color.rgb(255,255,200))
+        }
+        binding.openYellow.setOnClickListener {
+            openBox(Color.rgb(200,255,200))
+        }
+    }
+
+    private fun openBox(color:Int){
+        findNavController().navigate(R.id.action_rootFragment_to_boxFragment,
+        bundleOf(BoxFragment.ARG_COLOR to color)
+        )
+    }
+
 }
